5f20ad18e3aa96ec9b78db6aa2d2cdeb2ca1d98c
Remix Workspace <>
2022-10-25 02:57:58 -0500

    Lesson 4 Finished

--- contracts/FundMe_Upgraded.sol
+++ contracts/FundMe_Upgraded.sol
@@ -7,26 +7,37 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+// constant, immutable
+
+// 859817 gas
+// 840,281
+
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 20 * 1e18; 
+    uint256 public constant MINIMUM_USD = 20 * 1e18; 
+    // 21,415 - CONSTAnt
+    // 23,515 - non constant
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
+    // 21,508 - immutalbe
+    // 23,644 gas - non-immutable
 
     constructor(){
         // owner is whoever deployed this contract
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable { // in order to send smth with a function you need payable
         
         // Want to be able to set a mminimum fund amount in USD 
         // 1. How do we send ETH to this contract?
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 = 1 * 10^18
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!"); // 1e18 = 1 * 10^18
         
         // 18 decimals cus WEI
         // What is reverting?
@@ -58,18 +69,17 @@ contract FundMe {
         //call === IS THE BEST . this IS HOW WE SEND CRYPTO
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
+        revert();
      }
 
      modifier onlyOwner {
-        require(msg.sender == owner, "Sender is not owner. Wopz");
+        // require(msg.sender == i_owner, "Sender is not owner. Wopz");
+        
+        // more efficient (After 0.8.4)
+        if(msg.sender != i_owner){
+            revert NotOwner();
+        }
         _; // doing the rest of the code
      }
 
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
 }
